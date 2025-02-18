f29d4c33e0f41805a70353b53b552f81048a0ef2
hashim-baya <bayahashim40@gmail.com>
2025-02-19 00:30:36 +0300

    Enhanced responsiveness of the navbar

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link, NavLink } from 'react-router'
 import Logo from './Logo'
+import { Menu, X } from 'lucide-react'; 
 
 function NavBar() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen); 
+  }
+
+
   return (
     <div className='w-full '> 
 
-        <div className='flex justify-between bg-red-200 min-w-full p-4 flex-col items-center container sm:flex-row md:flex-row'   >
+        <div className='flex justify-between bg-red-200 min-w-full p-4 flex-col   container sm:flex-row md:flex-row'   >
            
-           <Logo height='10' width='200' />
+           <Logo height='10' width='200' className='mr-auto flex items-start' />
 
-            <div className='flex justify-between items-center mt-4 gap-4 sm:mt-0 '>
+           
+
+            <div className=' justify-between items-center mt-4 gap-4 sm:mt-0 hidden sm:flex ml-auto'>
                 
                 <NavLink to='/' className='text-red-400 font-extrabold text-center'>HOME</NavLink>
                 <NavLink to='/trending'className='text-red-400 font-extrabold text-center'>TRENDING</NavLink>
@@ -19,8 +30,30 @@ function NavBar() {
                 <NavLink to='/tvShows'className='text-red-400 font-extrabold text-center'>TV SHOWS</NavLink>
                 <NavLink to='#' className='text-purple-800 font-extrabold rounded-sm border-4 p-2 bg-blue-200 border-blue-50 hover:bg-slate-100 cursor-pointer ml-4 text-center'>LOGOUT</NavLink>
                
-                
             </div>
+
+            <button onClick={handleToggle} className='ml-auto sm:hidden absolute top-10 right-4' >
+              {isOpen ? <X size='30' /> : <Menu size='30' />}
+           </button>
+
+           
+
+           
+              
+                {isOpen && 
+                <div >
+                  <div className='flex flex-col gap-4 flex-wrap items-center justify-center mt-4  sm:hidden md:hidden'>
+
+                    <NavLink to='/' className='text-red-400 font-extrabold text-center'>HOME</NavLink>
+                    <NavLink to='/trending'className='text-red-400 font-extrabold text-center'>TRENDING</NavLink>
+                    <NavLink to='/tvSeries'className='text-red-400 font-extrabold text-center'>TV SERIES</NavLink>
+                    <NavLink to='/movies'className='text-red-400 font-extrabold text-center'>MOVIES</NavLink>
+                    <NavLink to='/tvShows'className='text-red-400 font-extrabold text-center'>TV SHOWS</NavLink>
+                    <NavLink to='#' className='text-purple-800 font-extrabold rounded-sm border-4 p-2 bg-blue-200 border-blue-50 hover:bg-slate-100 cursor-pointer ml-4 text-center'>LOGOUT</NavLink>
+                    
+                  </div>
+                </div>
+              }
 
         </div>
     </div>
